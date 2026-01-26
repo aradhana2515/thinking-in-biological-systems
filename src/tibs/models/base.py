e0a94bcb00141d1d3664405ea39fa1ed37798bc4
aradhana2515 <aradhana2515@gmail.com>
2026-01-26 09:15:37 +0530

    Create abstract Model class for biological models
    Defines an abstract base class for mechanistic biological models with required methods.

---
+++ src/tibs/models/base.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+class Model(ABC):
+    """
+    Base class for mechanistic biological models.
+    """
+
+    @abstractmethod
+    def initial_state(self):
+        """Return initial state vector."""
+        pass
+
+    @abstractmethod
+    def parameters(self):
+        """Return dict of model parameters."""
+        pass
+
+    @abstractmethod
+    def stoichiometry(self):
+        """Return stoichiometry matrix (N_species x N_reactions)."""
+        pass
+
+    @abstractmethod
+    def propensities(self, x, t, params):
+        """Return reaction propensities for Gillespie."""
+        pass
+
+    @abstractmethod
+    def rhs(self, t, x, params):
+        """ODE right-hand side."""
+        pass
+
+    def observe(self, x, params):
+        """Map internal state to observable (e.g. luminescence)."""
+        return x
