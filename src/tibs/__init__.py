5e3d2cba814a22ae94114b696b39959e3a3589a3
aradhana2515 <aradhana2515@gmail.com>
2026-02-21 08:26:28 +0530

    Update __all__ and import functions in __init__.py

--- src/tibs/__init__.py
+++ src/tibs/__init__.py
@@ -1,2 +1,5 @@
-__all__ = ["__version__"]
+from .features.timeseries import auc, peak, time_to_halfmax
+from .simulators.ssa import gillespie
+
+__all__ = ["__version__", "auc", "peak", "time_to_halfmax", "gillespie"]
 __version__ = "0.1.0"
