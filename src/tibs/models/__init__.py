03ef8db3ffd25679f6d8b88e6b3ec2a3d6e7eb4c
aradhana2515 <aradhana2515@gmail.com>
2026-02-22 00:36:01 +0530

    Uncomment imports and update __all__ list

--- src/tibs/models/__init__.py
+++ src/tibs/models/__init__.py
@@ -1,6 +1,7 @@
 from .base import Model, ODEModel, SimResult
 from .toggle_switch import ToggleSwitch
-# from .repressilator import Repressilator
-# from .sir import SIR
+from .repressilator import Repressilator
+from .sir import SIR
 
 __all__ = ["Model", "ODEModel", "SimResult", "ToggleSwitch"]
+
